4d3e7e9d4ba480da875820604065681dd16b8e32
Tom Tarpey <tomtarpeydev@gmail.com>
2020-06-07 22:38:08 +0100

    prep day 1

---
+++ singly_linked_list.py
@@ -0,0 +1,33 @@
+# create a Node Class
+class Node:
+    
+    def __init__(self, value=None, next_node=None):
+        pass
+
+    def get_value(self):
+        pass
+
+    def get_next(self):
+        pass
+
+    def set_next(self, new_node):
+        pass
+
+        
+
+# linked list class
+
+class LinkedList:
+    def __init__(self):
+        pass
+
+    def add_to_tail(self, value):
+        pass
+
+    def remove_head(self):
+        pass
+
+    def contains(self, value):
+        pass
+
+    
