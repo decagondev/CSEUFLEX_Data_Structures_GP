6f4ed90b132fcd9c1eb202ed74538bf988fee13e
Tom Tarpey <tomtarpeydev@gmail.com>
2020-06-10 08:36:52 +0100

    starter code for part 1 of day 3

---
+++ design_classes.py
@@ -0,0 +1,13 @@
+# lets design a 2d vector class set
+
+# Think about what data the class will hold
+# Think about what methods it needs
+# Think about what methods could be optional
+
+# Draw out diagrams of the class model
+
+# visualize how it will be formed
+
+# write out a basic structure
+
+# reflect on the design & think of improvements
